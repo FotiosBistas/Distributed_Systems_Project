caf69542cc03d886eb44dcdf107ab5656479262e
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-04-14 03:05:34 +0300

    Methods needed for the profile name functionality

--- src/ProfileName.java
+++ src/ProfileName.java
@@ -3,10 +3,19 @@ import java.util.HashMap;
 
 public class ProfileName {
     private String profileName;
-    private HashMap<String, ArrayList<MultimediaFile>> userVideoFilesMap;
+    private HashMap<String,ArrayList<MultimediaFile>> userVideoFilesMap;
     private HashMap<String,Integer> subscribedConversations;
-    ProfileName() {
 
-
+    ProfileName(String profileName) {
+        this.profileName = profileName;
     }
+
+    private void addToVideoFilesMap(String topic, MultimediaFile file){
+        userVideoFilesMap.get(topic).add(file);
+    }
+
+    private void addSubscription(String topic, int val){
+        subscribedConversations.put(topic,val);
+    }
+
 }
