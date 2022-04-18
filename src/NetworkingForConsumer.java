303ebc2ad6c5b0d08bf609a12bfdeb0dfdc40416
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-04-18 22:22:46 +0300

    fixed issue with exception when terminating client

--- src/NetworkingForConsumer.java
+++ src/NetworkingForConsumer.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
@@ -10,30 +11,37 @@ class NetworkingForConsumer implements Runnable{
     private ObjectInputStream is;
     private Socket request_socket;
     private Consumer cons;
-    boolean exit;
+    boolean exit = false;
     public NetworkingForConsumer(Socket request_socket,Consumer cons){
         this.request_socket = request_socket;
         this.cons = cons;
         try {
             os = new ObjectOutputStream(request_socket.getOutputStream());
             is = new ObjectInputStream(request_socket.getInputStream());
+
         } catch (IOException e) {
             e.printStackTrace();
+            killclient();
         }
     }
 
     public void killclient(){
         System.out.println("Ending client: " + cons.getName());
-        exit = true;
         try{
             if(is != null) {
+                System.out.println("Input stream was not null so we had to close it");
                 is.close();
+                is = null;
             }
             if(os != null) {
+                System.out.println("Output stream was not null so we had to close it");
                 os.close();
+                os = null;
             }
             if(request_socket != null) {
+                System.out.println("Socket was not closed so we had to close it");
                 request_socket.close();
+                request_socket = null;
             }
         }catch(IOException e){
             e.printStackTrace();
@@ -45,6 +53,9 @@ class NetworkingForConsumer implements Runnable{
         try{
             Scanner sc = new Scanner(System.in);
             System.out.println("I'm the client: " + cons.getName() + " and i have connected to the server");
+            //System.out.println("Requesting for broker list");
+            //os.writeUTF("GetBrokerList");
+            //os.flush();
             while(!exit) {
                 System.out.println("1.Register to topic");
                 System.out.println("2.Disconnect from topic");
@@ -75,24 +86,28 @@ class NetworkingForConsumer implements Runnable{
                         break;
                     case 3:
                         break;
+
                     case 0:
-                        System.out.println("Ending the connection with the server...");
+                        System.out.println("Terminating connection with server in run");
+                        killclient();
                         break;
                     default:
                         System.out.println("Invalid Request... Try again");
-                        killclient();
-                        continue;
+                        break;
                 }
             }
         } catch (UnknownHostException e) {
-            System.out.println("Terminating client...");
+            System.out.println("Terminating client networking run...");
+            killclient();
             e.printStackTrace();
         } catch (IOException e) {
-            System.out.println("Terminating client...");
+            System.out.println("Terminating client networking run...");
+            killclient();
             e.printStackTrace();
 
         }catch(Exception e){
-            System.out.println("Terminating client...");
+            System.out.println("Terminating client networking run...");
+            killclient();
             e.printStackTrace();
         }
     }
@@ -102,13 +117,17 @@ class NetworkingForConsumer implements Runnable{
             public void run() {
                 String messagebroker;
                 System.out.println("Opened thread to receive messages from broker");
-                while(request_socket.isConnected()){
+                while (!exit) {
                     System.out.println("Waiting...");
                     try {
                         messagebroker = is.readUTF();
                         System.out.println("Received message from broker: " + messagebroker);
-                    } catch (IOException e) {
-                        System.out.println("Terminating client...");
+                    }catch (SocketException e){
+                        System.out.println("Socket was closed before");
+                        killclient();
+                    }
+                    catch (Exception e) {
+                        System.out.println("Terminating client in broker responses...");
                         e.printStackTrace();
                         killclient();
                     }
