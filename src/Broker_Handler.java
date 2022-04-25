b0d0327fbc8dfc848e13df05a9d061836fae72ce
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-04-25 16:33:01 +0300

    new useful classes

---
+++ src/Broker_Handler.java
@@ -0,0 +1,204 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
+class Broker_Handler implements Runnable{
+
+    private Socket broker_connection;
+    private Broker broker;
+    private ObjectInputStream is;
+    private ObjectOutputStream os;
+
+    Broker_Handler(Socket broker_connection, Broker broker){
+        try{
+            this.broker_connection = broker_connection;
+            this.broker = broker;
+            is = new ObjectInputStream(broker_connection.getInputStream());
+            os = new ObjectOutputStream(broker_connection.getOutputStream());
+        }catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Error starting broker connection");
+            TerminateBrokerConnection();
+        }
+    }
+
+    public void receiveTopicList(){
+
+    }
+
+    public void receiveBrokerList() {
+        try {
+            int messagebroker = -1;
+            while (true) {
+                System.out.println("In while loop for the broker list...");
+                if ((messagebroker == Messages.FINISHED_OPERATION.ordinal())) {
+                    System.out.println("Received finished operation message in the while loop for the broker list");
+                    break;
+                }
+                int size = is.readInt();
+                System.out.println("Size of list: " + size);
+                String ip = is.readUTF();
+                System.out.println("Broker's ip: " + ip);
+                System.out.println("Sending broker's ports...");
+                ArrayList<Integer> temp = new ArrayList<>();
+                while (true) {
+                    System.out.println("In while loop for the broker's ports...");
+                    if ((messagebroker == Messages.FINISHED_OPERATION.ordinal())) {
+                        System.out.println("Received finished operation message in the while loop for the broker's ports");
+                        break;
+                    }
+                    int port = is.readInt();
+                    System.out.println("Received port: " + port);
+                    temp.add(port);
+                    if (temp.size() >= 3) {
+                        System.out.println("Waiting for finished operation message by the broker in the while loop for sending port array");
+                        messagebroker = readInput();
+                    }
+                }
+                int[] ports = new int[3];
+                ports[0] = temp.get(0);
+                ports[1] = temp.get(1);
+                ports[2] = temp.get(2);
+                Tuple<String,int[]> new_broker = new Tuple<String, int[]>(ip, ports);
+                boolean exists = false;
+                for (Tuple<String, int[]> val:broker.getBrokerList()) {
+                    if(val.getValue1().equals(new_broker.getValue1())) {
+                        for (int i = 0; i < val.getValue2().length; i++) {
+                            if (val.getValue2()[i] == ports[i]) {
+                                exists = true;
+                            }
+                        }
+                    }else{
+                        exists = true;
+                    }
+                }
+                if(!exists){
+                    broker.getBrokerList().add(new_broker);
+                }
+                if (broker.getBrokerList().size() >= size) {
+                    System.out.println("Waiting for finished operation message by the broker in the while loop for sending broker list");
+                    messagebroker = readInput();
+                }
+                System.out.println("Broker received broker list");
+                FinishedOperation();
+            }
+        } catch (IOException e) {
+            System.out.println("Error in receiving broker list");
+            e.printStackTrace();
+            TerminateBrokerConnection();
+        }
+    }
+
+    public void FinishedOperation(){
+        try {
+            os.writeInt(Messages.FINISHED_OPERATION.ordinal());
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Shutting down connection in finished operation...");
+            TerminateBrokerConnection();
+        }
+    }
+
+    public Integer readInput(){
+        try{
+            System.out.println("Waiting to receive message by broker: " + broker_connection.getInetAddress());
+            return is.readInt();
+        }catch(IOException e){
+            e.printStackTrace();
+            System.out.println("Error in read input");
+            TerminateBrokerConnection();
+        }
+        return null;
+    }
+
+    @Override
+    public void run() {
+        int message_from_broker = readInput();
+        while(!broker_connection.isClosed()){
+           if(message_from_broker == Messages.FINISHED_OPERATION.ordinal()){
+               System.out.println("Waiting for next input from broker");
+               message_from_broker = readInput();
+           }else if(message_from_broker == Messages.SENDING_BROKER_LIST.ordinal()){
+                receiveBrokerList();
+                receiveIdList();
+                message_from_broker = readInput();
+           }else if(message_from_broker == Messages.NEW_TOPIC.ordinal()){
+                receiveTopicList();
+                message_from_broker = readInput();
+           }else if(message_from_broker == Messages.CLOSE_CONNECTION.ordinal()){
+               System.out.println("Closing connection...");
+               TerminateBrokerConnection();
+               break;
+           }else if(message_from_broker == Messages.WAITING_FOR_ACK.ordinal()){
+               System.out.println("Sending ACK");
+               sendACK();
+               message_from_broker = readInput();
+           }
+        }
+    }
+
+    private void receiveIdList() {
+        System.out.println("Broker is sending its ID List");
+        try {
+            int messagebroker = -1;
+            while (true) {
+                if ((messagebroker == Messages.FINISHED_OPERATION.ordinal())) {
+                    System.out.println("Received finished operation message in while loop sending id list");
+                    break;
+                }
+                int size = is.readInt();
+                System.out.println("Received size: " + size);
+                int id = is.readInt();
+                System.out.println("Received ID: " + id);
+                broker.getId_list().add(id);
+                if (broker.getId_list().size() >= size) {
+                    System.out.println("Waiting for finished operation message by the broker in the while loop for sending id list");
+                    messagebroker = readInput();
+                }
+            }
+            System.out.println("Broker sent the id list");
+            os.writeInt(Messages.FINISHED_OPERATION.ordinal());
+            os.flush();
+        }catch (IOException e){
+            System.out.println("Error in sending id list");
+            e.printStackTrace();
+            TerminateBrokerConnection();
+        }
+    }
+
+    public void sendACK(){
+        try{
+            System.out.println("Sending message type: " + Messages.ACK + " with ordinal number: " + Messages.ACK.ordinal());
+            os.writeInt(Messages.ACK.ordinal());
+            os.flush();
+        }catch (IOException e){
+            System.out.println("Error in sending ack");
+            e.printStackTrace();
+            TerminateBrokerConnection();
+        }
+    }
+
+    public void TerminateBrokerConnection(){
+        System.out.println("Shutting down broker connection...");
+        try {
+            if(broker_connection != null) {
+                broker_connection.close();
+            }
+            if(is != null){
+                is.close();
+            }
+            if(os != null){
+                os.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error in shutting down connection");
+        }
+
+
+    }
+
+}
---
+++ src/Shared_Network_Methods.java
@@ -0,0 +1,127 @@
+import java.io.IOException;
+
+class Shared_Network_Methods{
+
+
+    /**
+     * Accepts the object that is requesting for the broker list.
+     * After that it sends the broker list using the appropriate streams and sockets.
+     * @param caller
+     */
+    public static void sendBrokerList(Object caller){
+        System.out.println("Sending broker list");
+        if(caller instanceof Broker){
+            Broker broker = (Broker) caller;
+            try {
+                System.out.println("Sending message type: " + Messages.SENDING_BROKER_LIST + " with ordinal number: " + Messages.SENDING_BROKER_LIST.ordinal());
+                broker.getLocaloutputStream().writeInt(Messages.SENDING_BROKER_LIST.ordinal());
+                broker.getLocaloutputStream().flush();
+                for (Tuple<String, int[]> val : broker.getBrokerList()) {
+                    System.out.println("Sending Broker List size: " + broker.getBrokerList().size());
+                    broker.getLocaloutputStream().writeInt(broker.getBrokerList().size());
+                    broker.getLocaloutputStream().flush();
+                    System.out.println("Sending broker's IP: " + val.getValue1());
+                    broker.getLocaloutputStream().writeUTF(val.getValue1());
+                    broker.getLocaloutputStream().flush();
+                    int i;
+                    for (i = 0; i < val.getValue2().length; i++) {
+                        System.out.println("Sending broker's ports: " + val.getValue2()[i]);
+                        broker.getLocaloutputStream().writeInt(val.getValue2()[i]);
+                        broker.getLocaloutputStream().flush();
+                    }
+                    if (i == 3) {
+                        System.out.println("Finished sending ports");
+                        broker.FinishedOperation();
+                    }
+                }
+            }
+            catch (IOException e){
+
+            }
+        }else if(caller instanceof Consumer_Handler){
+            Consumer_Handler handler = (Consumer_Handler) caller;
+            try{
+                System.out.println("Sending message type: " + Messages.SENDING_BROKER_LIST + " with ordinal number: " + Messages.SENDING_BROKER_LIST.ordinal());
+                handler.getLocaloutputStream().writeInt(Messages.SENDING_BROKER_LIST.ordinal());
+                handler.getLocaloutputStream().flush();
+                for (Tuple<String, int[]> val : handler.getBroker().getBrokerList()) {
+                    System.out.println("Sending Broker List size: " + handler.getBroker().getBrokerList().size());
+                    handler.getLocaloutputStream().writeInt(handler.getBroker().getBrokerList().size());
+                    handler.getLocaloutputStream().flush();
+                    System.out.println("Sending broker's IP: " + val.getValue1());
+                    handler.getLocaloutputStream().writeUTF(val.getValue1());
+                    handler.getLocaloutputStream().flush();
+                    int i;
+                    for (i = 0; i < val.getValue2().length; i++) {
+                        System.out.println("Sending broker's ports: " + val.getValue2()[i]);
+                        handler.getLocaloutputStream().writeInt(val.getValue2()[i]);
+                        handler.getLocaloutputStream().flush();
+                    }
+                    if (i == 3) {
+                        System.out.println("Finished sending ports");
+                        handler.FinishedOperation();
+                    }
+                }
+            }
+            catch (IOException e){
+                e.printStackTrace();
+                System.out.println("Shutting down connection in send broker list...");
+                handler.shutdownConnection();
+            }
+        }
+    }
+
+    /**
+     * Accepts the object that is requesting for the broker list.
+     * After that it sends the broker list using the appropriate streams and sockets.
+     * @param caller
+     */
+    public static void sendIdList(Object caller){
+        System.out.println("Sending ID List");
+        if(caller instanceof Broker) {
+            Broker broker = (Broker) caller;
+            try {
+                System.out.println("Sending message type: " + Messages.SENDING_ID_LIST + " with ordinal number: " + Messages.SENDING_ID_LIST.ordinal());
+                broker.getLocaloutputStream().writeInt(Messages.SENDING_ID_LIST.ordinal());
+                broker.getLocaloutputStream().flush();
+                for (int i = 0; i < broker.getId_list().size(); i++) {
+                    System.out.println("Sending ID List size: " + broker.getId_list().size());
+                    broker.getLocaloutputStream().writeInt(broker.getId_list().size());
+                    broker.getLocaloutputStream().flush();
+                    System.out.println("Sending ID: " + broker.getId_list().get(i));
+                    broker.getLocaloutputStream().writeInt(broker.getId_list().get(i));
+                    broker.getLocaloutputStream().flush();
+                }
+            } catch (IOException e) {
+                System.out.println("Error in sending list");
+                e.printStackTrace();
+                broker.shutdownConnection();
+            }
+        }
+        else if(caller instanceof Consumer_Handler){
+            Consumer_Handler handler = (Consumer_Handler) caller;
+            try {
+                System.out.println("Sending message type: " + Messages.SENDING_ID_LIST + " with ordinal number: " + Messages.SENDING_ID_LIST.ordinal());
+                handler.getLocaloutputStream().writeInt(Messages.SENDING_ID_LIST.ordinal());
+                handler.getLocaloutputStream().flush();
+                for (int i = 0; i < handler.getBroker().getId_list().size(); i++) {
+                    System.out.println("Sending ID List size: " + handler.getBroker().getId_list().size());
+                    handler.getLocaloutputStream().writeInt(handler.getBroker().getId_list().size());
+                    handler.getLocaloutputStream().flush();
+                    System.out.println("Sending ID: " + handler.getBroker().getId_list().get(i));
+                    handler.getLocaloutputStream().writeInt(handler.getBroker().getId_list().get(i));
+                    handler.getLocaloutputStream().flush();
+                }
+            } catch (IOException e) {
+                System.out.println("Error in sending list");
+                e.printStackTrace();
+                handler.shutdownConnection();
+            }
+        }
+    }
+
+    public static void sendTopicList(){
+
+    }
+
+}
