62df9fc4bd504a484fa709c68eb6ae1ba328ec90
FotiosBistas <83087431+FotiosBistas@users.noreply.github.com>
2022-05-02 17:29:42 +0300

    text message class

---
+++ src/Tools/Text_Message.java
@@ -0,0 +1,28 @@
+package Tools;
+
+import java.io.Serializable;
+
+public class Text_Message extends Value implements Serializable {
+    private final String contents;
+
+    public String getContents() {
+        return contents;
+    }
+
+    public Text_Message(String date_created, String publisher, String contents){
+        super(publisher,date_created);
+        this.contents = contents;
+    }
+
+    public Text_Message(String publisher,String contents) {
+        super(publisher);
+        this.contents = contents;
+    }
+
+    @Override
+    public String toString() {
+        return " Text_Message{" +
+                "contents='" + contents + '\'' +
+                '}';
+    }
+}
