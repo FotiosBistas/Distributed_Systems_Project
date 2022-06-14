c4ed0e36eb1b2140ff61c8496639338f8441d121
Fotis Bistas <83087431+FotiosBistas@users.noreply.github.com>
2022-06-14 03:05:38 +0300

    Add files via upload

--- android/ChitChat/app/src/main/java/com/example/chitchat/Menu_Activity.java
+++ android/ChitChat/app/src/main/java/com/example/chitchat/Menu_Activity.java
@@ -3,12 +3,14 @@ package com.example.chitchat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class Menu_Activity extends AppCompatActivity {
+public class Menu_Activity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +30,22 @@ public class Menu_Activity extends AppCompatActivity {
             startActivity(new Intent(Menu_Activity.this, Profile_Activity.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.register_button:
+                break;
+            case R.id.disconnect_button:
+                break;
+            case R.id.upload_file:
+                break;
+            case R.id.upload_image:
+                break;
+            case R.id.upload_story:
+                break;
+        }
     }
 }
